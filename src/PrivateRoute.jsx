03a6e31e2bcd9c47bcaa24442f5b0ce9fc84a74a
Ken Lacanienta <kenlacanienta93@gmail.com>
2025-03-05 18:56:14 +0800

    PrivateRoute changes

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,8 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-const PrivateRoute = () => {
-  const isAuthenticated = localStorage.getItem("token"); // Check if user is logged in
-    return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+const PrivateRoute = ({ children }) => {
+  const isAuthenticated = localStorage.getItem("token");
+  return isAuthenticated ? children : <Navigate to="/" />;
 };
+
 
 export default PrivateRoute;

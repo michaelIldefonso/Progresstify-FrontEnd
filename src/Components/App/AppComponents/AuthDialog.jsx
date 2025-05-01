a94b0cd812e4510098d595efa5feec2cc7505f63
Okims2231 <irenearknights2231@gmail.com>
2025-05-01 14:42:08 +0800

    login popup rework

--- src/Components/App/AppComponents/AuthDialog.jsx
+++ src/Components/App/AppComponents/AuthDialog.jsx
@@ -1,25 +1,20 @@
-import { useState } from "react";
 import {
   Dialog,
   DialogTitle,
   DialogContent,
   Container,
   Paper,
-  TextField,
   Button,
-  Typography,
-  Checkbox,
-  FormControlLabel,
 } from "@mui/material";
 import GoogleIcon from "@mui/icons-material/Google";
-import { handleChange, handleSubmit } from "../../Functions/eventHandlerFunctions";
 import PropTypes from "prop-types"; // Import PropTypes
+import FacebookIcon from "@mui/icons-material/Facebook"; // Import FacebookIcon
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 function AuthDialog({ open, setOpen, isSignUp, setIsSignUp, darkMode }) {
-  const [formData, setFormData] = useState({ email: "", password: "", confirmPassword: "" });
-
   return (
     <Dialog
       open={open}
@@ -44,113 +39,81 @@ function AuthDialog({ open, setOpen, isSignUp, setIsSignUp, darkMode }) {
               backgroundColor: "transparent",
             }}
           >
-            <form onSubmit={(e) => handleSubmit(e, isSignUp, formData, setOpen)}>
-              <TextField
-                fullWidth
-                margin="normal"
-                label="Email"
-                name="email"
-                type="email"
-                variant="filled"
-                InputProps={{
-                  style: {
-                    backgroundColor: darkMode ? "#1f2937" : "#f1bb75",
-                    color: darkMode ? "#fff" : "#000",
-                  },
-                }}
-                InputLabelProps={{
-                  style: { color: darkMode ? "#578FCA" : "black" },
-                }}
-                value={formData.email}
-                onChange={(e) => handleChange(e, formData, setFormData)}
-                required
-              />
-              <TextField
-                fullWidth
-                margin="normal"
-                label="Password"
-                name="password"
-                type="password"
-                variant="filled"
-                InputProps={{
-                  style: {
-                    backgroundColor: darkMode ? "#1f2937" : "#f1bb75",
-                    color: darkMode ? "#fff" : "#000",
-                  },
-                }}
-                InputLabelProps={{
-                  style: { color: darkMode ? "#578FCA" : "black" },
-                }}
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-              {isSignUp && (
-                <TextField
-                  fullWidth
-                  margin="normal"
-                  label="Confirm Password"
-                  name="confirmPassword"
-                  type="password"
-                  variant="filled"
-                  InputProps={{
-                    style: {
-                      backgroundColor: darkMode ? "#1f2937" : "#e0e0e0",
-                      color: darkMode ? "#fff" : "#000",
-                    },
-                  }}
-                  InputLabelProps={{ style: { color: "#578FCA" } }}
-                  value={formData.confirmPassword}
-                  onChange={handleChange}
-                  required
-                />
-              )}
-              <FormControlLabel
-                control={<Checkbox style={{ color: darkMode ? "white" : "black" }} />}
-                label={
-                  <Typography variant="body2" style={{ color: darkMode ? "white" : "black" }}>
-                    Remember me
-                  </Typography>
-                }
-              />
-              <Button
-                fullWidth
-                variant="contained"
-                sx={{
-                  backgroundColor: darkMode ? "#578FCA" : "#f88f4e",
-                  color: darkMode ? "#000" : "black",
-                  marginTop: "10px",
-                }}
-                type="submit"
-              >
-                {isSignUp ? "Sign Up" : "Sign In"}
-              </Button>
-            </form>
-            <Typography
-              variant="body2"
-              sx={{ marginTop: "10px", color: darkMode ? "#9ca3af" : "#757575" }}
-            >
-              Forgot your password?
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ marginY: "10px", color: darkMode ? "#9ca3af" : "#757575" }}
-            >
-              or
-            </Typography>
+
             <Button
               fullWidth
               onClick={() => window.open(`${API_BASE_URL}/auth/google`, "_self")}
               variant="outlined"
               startIcon={<GoogleIcon />}
               sx={{
+                fontSize: "1.25rem", 
+                padding: "12px 16px", 
+                height: "50px", 
                 color: darkMode ? "white" : "black",
                 borderColor: darkMode ? "white" : "black",
                 textTransform: "none",
+                marginBottom: "10px",
               }}
             >
               Sign in with Google
             </Button>
+
+            <Button
+              fullWidth
+              onClick={() => window.open(`${API_BASE_URL}/auth/facrbook`, "_self")}
+              variant="outlined"
+              startIcon={<FacebookIcon />}
+              sx={{
+                fontSize: "1.25rem", 
+                padding: "12px 16px", 
+                height: "50px",
+                color: darkMode ? "white" : "black",
+                borderColor: darkMode ? "white" : "black",
+                textTransform: "none",
+                marginBottom: "10px",
+              }}
+            >
+              Sign in with Facbook
+            </Button>
+
+            <Button
+              fullWidth
+              onClick={() => window.open(`${API_BASE_URL}/auth/GitHub`, "_self")}
+              variant="outlined"
+              startIcon={<GitHubIcon />}
+              sx={{
+                fontSize: "1.25rem", 
+                padding: "12px 16px", 
+                height: "50px",
+                color: darkMode ? "white" : "black",
+                borderColor: darkMode ? "white" : "black",
+                textTransform: "none",
+                marginBottom: "10px",
+              }}
+            >
+              Sign in with Github
+            </Button>
+
+            <Button
+              fullWidth
+              onClick={() => window.open(`${API_BASE_URL}/auth/Twitter`, "_self")}
+              variant="outlined"
+              startIcon={<TwitterIcon />}
+              sx={{
+                fontSize: "1.25rem", 
+                padding: "12px 16px", 
+                height: "50px",
+                color: darkMode ? "white" : "black",
+                borderColor: darkMode ? "white" : "black",
+                textTransform: "none",
+                marginBottom: "10px",
+              }}
+            >
+              Sign in with Twitter
+            </Button>
+
+
+
             <Button
               fullWidth
               color="secondary"
@@ -171,6 +134,7 @@ function AuthDialog({ open, setOpen, isSignUp, setIsSignUp, darkMode }) {
     </Dialog>
   );
 }
+
 // Add PropTypes for validation
 AuthDialog.propTypes = {
   open: PropTypes.bool.isRequired,
@@ -179,4 +143,5 @@ AuthDialog.propTypes = {
   setIsSignUp: PropTypes.func.isRequired,
   darkMode: PropTypes.bool.isRequired,
 };
+
 export default AuthDialog;

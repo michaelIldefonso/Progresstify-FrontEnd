2e9f376865d62aafdb174d2a6373745a8a40fd13
Okims2231 <irenearknights2231@gmail.com>
2025-05-05 14:54:32 +0800

    AppbarWithMenu.jsx debug

--- src/Components/mainComponents/AppbarWithMenu.jsx
+++ src/Components/mainComponents/AppbarWithMenu.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import { AppBar, Toolbar, Button, Menu, MenuItem, IconButton, Typography } from "@mui/material";
-import { Brightness4, Brightness7, Menu as MenuIcon } from "@mui/icons-material";
+import { Brightness4, Brightness7, } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { handleMenu, handleClose } from "../Functions/eventHandlerFunctions";
 import { handleLogout } from "../Functions/navigationFunctions";
+import PropTypes from "prop-types";
 
 const AppBarWithMenu = ({ darkMode, toggleDarkMode, anchorEl, setAnchorEl, user }) => {
   const navigate = useNavigate();
@@ -59,4 +59,11 @@ const AppBarWithMenu = ({ darkMode, toggleDarkMode, anchorEl, setAnchorEl, user 
   );
 };
 
+AppBarWithMenu.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  toggleDarkMode: PropTypes.func.isRequired,
+  anchorEl: PropTypes.object,
+  setAnchorEl: PropTypes.func.isRequired,
+  user: PropTypes.object,
+};
 export default AppBarWithMenu;
